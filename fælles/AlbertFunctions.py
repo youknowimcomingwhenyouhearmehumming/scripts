fbfe0138b01f966364b5b0cee6872d0b5cd574c1
Sorck228 <42936273+Sorck228@users.noreply.github.com>
2019-10-09 14:22:34 +0200

    Ups

--- fælles/AlbertFunctions.py
+++ fælles/AlbertFunctions.py
@@ -70,7 +70,7 @@ def video_export_v2(output_img_folder,images):
             height, width, = img.shape 
             size = (width,height)
             img_array.append(img)
-        out = cv2.VideoWriter(r"/ThorHarEnLilleDiller.avi",cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+        out = cv2.VideoWriter(r"ThorHarEnLilleDiller.avi",cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
         
         for i in range(len(img_array)):
             out.write(img_array[i])
