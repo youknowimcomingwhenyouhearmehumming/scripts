4e9567c3169b3324bb814b1511b568210b2f92c5
youknowimcomingwhenyouhearmehumming <31764934+youknowimcomingwhenyouhearmehumming@users.noreply.github.com>
2019-10-09 13:00:32 +0200

    thor
    thor

---
+++ thor/test3.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import imutils
+import math
+
+
+os.chdir('C:/Users/Bruger/Documents/Uni/Abu dhabi/data/outdoor')
+
+print('start')
+
+b=3
+
+# Read image
+im = cv2.imread('pica36.png')
+imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+im_gauss = cv2.GaussianBlur(imgray, (5, 5), 0)
+ret, thresh = cv2.threshold(im_gauss, 127, 255, 0)
+# get contours
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+contours_area = []
+# calculate area and filter into new array
+for con in contours:
+    area = cv2.contourArea(con)
+    if 10 < area < 10000:
+        contours_area.append(con)
+        
+        
+contours_cirles = []
+
+# check if contour is of circular shape
+for con in contours_area:
+    perimeter = cv2.arcLength(con, True)
+    area = cv2.contourArea(con)
+    if perimeter == 0:
+        break
+    circularity = 4*math.pi*(area/(perimeter*perimeter))
+    print (circularity)
+    if 0.1 < circularity < 1.2:
+        contours_cirles.append(con)
+        
+        
+cv2.drawContours(im, contours, -1, (0, 255, 0), 3) 
+  
+cv2.imshow('Contours', im) 
+#cv2.waitKey(0) 
+#cv2.destroyAllWindows() 
---
+++ thor/test45py.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import imutils
+import math
+
+
+os.chdir('C:/Users/Bruger/Documents/Uni/Abu dhabi/data/outdoor')
+
+print('start')
+
+b=3
+
+# Read image
+img = cv2.imread('pica36.png')
+
+
+#img = cv2.imread('blob.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blurred = cv2.medianBlur(gray, 9)
+_filter = cv2.bilateralFilter(blurred, 5, 75, 75)
+adap_thresh = cv2.adaptiveThreshold(_filter,
+                                    255,
+                                    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                    cv2.THRESH_BINARY_INV,
+                                    21, 0)
+
+element = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3)) 
+dilated = cv2.dilate(adap_thresh, element, iterations=1)
+
+# blob detection
+params = cv2.SimpleBlobDetector_Params()
+params.filterByColor = False
+params.minThreshold = 80
+params.maxThreshold = 93
+params.blobColor = 0
+params.minArea = 200
+params.maxArea = 250
+params.filterByCircularity = False
+params.filterByConvexity = True
+params.minCircularity =0.0000001
+params.maxCircularity = 1
+
+det = cv2.SimpleBlobDetector_create(params)
+keypts = det.detect(dilated)
+
+im_with_keypoints = cv2.drawKeypoints(dilated,
+                                      keypts,
+                                      np.array([]),
+                                      (0, 0, 255),
+                                      cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+res = cv2.drawKeypoints(img,
+                        keypts,
+                        np.array([]),
+                        (0, 0, 255 ),
+                        cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+i = 0
+for kp in keypts:
+    print("(%f,%f)"%(kp.pt[0],kp.pt[1]))
+    i+=1
+    cv2.rectangle(res,(int(kp.pt[0]),int(kp.pt[1])),(int(kp.pt[0])+1,int(kp.pt[1])+1),(0,255,0),2)
+
+#cv2.imshow("Keypoints", im_with_keypoints)
+cv2.imshow("RES", res)
+cv2.waitKey(0)
